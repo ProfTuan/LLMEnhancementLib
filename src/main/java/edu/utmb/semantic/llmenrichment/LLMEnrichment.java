d3a1db07a3eac51950f2832ebeacd893bad9cbb4
ProfTuan <muhammad.f.amith@uth.tmc.edu>
2024-06-21 06:02:20 -0500

    init

---
+++ src/main/java/edu/utmb/semantic/llmenrichment/LLMEnrichment.java
@@ -0,0 +1,13 @@
+package edu.utmb.semantic.llmenrichment;
+
+/**
+ * Hello world!
+ *
+ */
+public class LLMEnrichment 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
